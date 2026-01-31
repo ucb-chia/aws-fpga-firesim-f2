f6a12c787252b0d17124fcbdb5f0e8a21c3437c7
Rick <rickhuangty@berkeley.edu>
2026-01-31 00:38:16 +0000

    rh: removed ILA again because it's not obedient

--- hdk/cl/developer_designs/cl_firesim/design/cl_firesim.sv
+++ hdk/cl/developer_designs/cl_firesim/design/cl_firesim.sv
@@ -1482,127 +1482,128 @@ logic zeroila;
 assign zeroila = 64'b0;
 
 // Integrated Logic Analyzers (ILA)
-ila_0 CL_ILA_0 (
-                  .clk    (clk_main_a0),
-                  .probe0 (zeroila),
-                  .probe1 (zeroila),
-                  .probe2 (zeroila),
-                  .probe3 (zeroila),
-                  .probe4 (zeroila),
-                  .probe5 (zeroila)
-                  );
+// rh: ILA coming soon! or it breaks.
+// ila_0 CL_ILA_0 (
+//                   .clk    (clk_main_a0),
+//                   .probe0 (zeroila),
+//                   .probe1 (zeroila),
+//                   .probe2 (zeroila),
+//                   .probe3 (zeroila),
+//                   .probe4 (zeroila),
+//                   .probe5 (zeroila)
+//                   );
 
-ila_0 CL_ILA_1 (
-                  .clk    (clk_main_a0),
-                  .probe0 (zeroila),
-                  .probe1 (zeroila),
-                  .probe2 (zeroila),
-                  .probe3 (zeroila),
-                  .probe4 (zeroila),
-                  .probe5 (zeroila)
-                  );
+// ila_0 CL_ILA_1 (
+//                   .clk    (clk_main_a0),
+//                   .probe0 (zeroila),
+//                   .probe1 (zeroila),
+//                   .probe2 (zeroila),
+//                   .probe3 (zeroila),
+//                   .probe4 (zeroila),
+//                   .probe5 (zeroila)
+//                   );
 
-// Debug Bridge 
-cl_debug_bridge CL_DEBUG_BRIDGE (
-   .clk(clk_main_a0),
-   .S_BSCAN_drck(drck),
-   .S_BSCAN_shift(shift),
-   .S_BSCAN_tdi(tdi),
-   .S_BSCAN_update(update),
-   .S_BSCAN_sel(sel),
-   .S_BSCAN_tdo(tdo),
-   .S_BSCAN_tms(tms),
-   .S_BSCAN_tck(tck),
-   .S_BSCAN_runtest(runtest),
-   .S_BSCAN_reset(reset),
-   .S_BSCAN_capture(capture),
-   .S_BSCAN_bscanid_en(bscanid_en)
-);
+// // Debug Bridge 
+// cl_debug_bridge CL_DEBUG_BRIDGE (
+//    .clk(clk_main_a0),
+//    .S_BSCAN_drck(drck),
+//    .S_BSCAN_shift(shift),
+//    .S_BSCAN_tdi(tdi),
+//    .S_BSCAN_update(update),
+//    .S_BSCAN_sel(sel),
+//    .S_BSCAN_tdo(tdo),
+//    .S_BSCAN_tms(tms),
+//    .S_BSCAN_tck(tck),
+//    .S_BSCAN_runtest(runtest),
+//    .S_BSCAN_reset(reset),
+//    .S_BSCAN_capture(capture),
+//    .S_BSCAN_bscanid_en(bscanid_en)
+// );
 
-//-----------------------------------------------
-// VIO Example - Needs Virtual JTAG
-//-----------------------------------------------
-// Counter running at 125MHz
+// //-----------------------------------------------
+// // VIO Example - Needs Virtual JTAG
+// //-----------------------------------------------
+// // Counter running at 125MHz
 
-logic      vo_cnt_enable;
-logic      vo_cnt_load;
-logic      vo_cnt_clear;
-logic      vo_cnt_oneshot;
-logic [7:0]  vo_tick_value;
-logic [15:0] vo_cnt_load_value;
-logic [15:0] vo_cnt_watermark;
+// logic      vo_cnt_enable;
+// logic      vo_cnt_load;
+// logic      vo_cnt_clear;
+// logic      vo_cnt_oneshot;
+// logic [7:0]  vo_tick_value;
+// logic [15:0] vo_cnt_load_value;
+// logic [15:0] vo_cnt_watermark;
 
-logic      vo_cnt_enable_q = 0;
-logic      vo_cnt_load_q = 0;
-logic      vo_cnt_clear_q = 0;
-logic      vo_cnt_oneshot_q = 0;
-logic [7:0]  vo_tick_value_q = 0;
-logic [15:0] vo_cnt_load_value_q = 0;
-logic [15:0] vo_cnt_watermark_q = 0;
+// logic      vo_cnt_enable_q = 0;
+// logic      vo_cnt_load_q = 0;
+// logic      vo_cnt_clear_q = 0;
+// logic      vo_cnt_oneshot_q = 0;
+// logic [7:0]  vo_tick_value_q = 0;
+// logic [15:0] vo_cnt_load_value_q = 0;
+// logic [15:0] vo_cnt_watermark_q = 0;
 
-logic        vi_tick;
-logic        vi_cnt_ge_watermark;
-logic [7:0]  vi_tick_cnt = 0;
-logic [15:0] vi_cnt = 0;
+// logic        vi_tick;
+// logic        vi_cnt_ge_watermark;
+// logic [7:0]  vi_tick_cnt = 0;
+// logic [15:0] vi_cnt = 0;
 
-// Tick counter and main counter
-always @(posedge clk_main_a0) begin
+// // Tick counter and main counter
+// always @(posedge clk_main_a0) begin
 
-   vo_cnt_enable_q     <= vo_cnt_enable    ;
-   vo_cnt_load_q       <= vo_cnt_load      ;
-   vo_cnt_clear_q      <= vo_cnt_clear     ;
-   vo_cnt_oneshot_q    <= vo_cnt_oneshot   ;
-   vo_tick_value_q     <= vo_tick_value    ;
-   vo_cnt_load_value_q <= vo_cnt_load_value;
-   vo_cnt_watermark_q  <= vo_cnt_watermark ;
+//    vo_cnt_enable_q     <= vo_cnt_enable    ;
+//    vo_cnt_load_q       <= vo_cnt_load      ;
+//    vo_cnt_clear_q      <= vo_cnt_clear     ;
+//    vo_cnt_oneshot_q    <= vo_cnt_oneshot   ;
+//    vo_tick_value_q     <= vo_tick_value    ;
+//    vo_cnt_load_value_q <= vo_cnt_load_value;
+//    vo_cnt_watermark_q  <= vo_cnt_watermark ;
 
-   vi_tick_cnt = vo_cnt_clear_q ? 0 :
-                  ~vo_cnt_enable_q ? vi_tick_cnt :
-                  (vi_tick_cnt >= vo_tick_value_q) ? 0 :
-                  vi_tick_cnt + 1;
+//    vi_tick_cnt = vo_cnt_clear_q ? 0 :
+//                   ~vo_cnt_enable_q ? vi_tick_cnt :
+//                   (vi_tick_cnt >= vo_tick_value_q) ? 0 :
+//                   vi_tick_cnt + 1;
 
-   vi_cnt = vo_cnt_clear_q ? 0 :
-            vo_cnt_load_q ? vo_cnt_load_value_q :
-            ~vo_cnt_enable_q ? vi_cnt :
-            (vi_tick_cnt >= vo_tick_value_q) && (~vo_cnt_oneshot_q || (vi_cnt <= 16'hFFFF)) ? vi_cnt + 1 :
-            vi_cnt;
+//    vi_cnt = vo_cnt_clear_q ? 0 :
+//             vo_cnt_load_q ? vo_cnt_load_value_q :
+//             ~vo_cnt_enable_q ? vi_cnt :
+//             (vi_tick_cnt >= vo_tick_value_q) && (~vo_cnt_oneshot_q || (vi_cnt <= 16'hFFFF)) ? vi_cnt + 1 :
+//             vi_cnt;
 
-   vi_tick = (vi_tick_cnt >= vo_tick_value_q);
+//    vi_tick = (vi_tick_cnt >= vo_tick_value_q);
 
-   vi_cnt_ge_watermark = (vi_cnt >= vo_cnt_watermark_q);
+//    vi_cnt_ge_watermark = (vi_cnt >= vo_cnt_watermark_q);
    
-end // always @ (posedge clk_main_a0)
+// end // always @ (posedge clk_main_a0)
 
 
-vio_0 CL_VIO_0 (
-                  .clk    (clk_main_a0),
-                  .probe_in0  (vi_tick),
-                  .probe_in1  (vi_cnt_ge_watermark),
-                  .probe_in2  (vi_tick_cnt),
-                  .probe_in3  (vi_cnt),
-                  .probe_out0 (vo_cnt_enable),
-                  .probe_out1 (vo_cnt_load),
-                  .probe_out2 (vo_cnt_clear),
-                  .probe_out3 (vo_cnt_oneshot),
-                  .probe_out4 (vo_tick_value),
-                  .probe_out5 (vo_cnt_load_value),
-                  .probe_out6 (vo_cnt_watermark)
-                  );
+// vio_0 CL_VIO_0 (
+//                   .clk    (clk_main_a0),
+//                   .probe_in0  (vi_tick),
+//                   .probe_in1  (vi_cnt_ge_watermark),
+//                   .probe_in2  (vi_tick_cnt),
+//                   .probe_in3  (vi_cnt),
+//                   .probe_out0 (vo_cnt_enable),
+//                   .probe_out1 (vo_cnt_load),
+//                   .probe_out2 (vo_cnt_clear),
+//                   .probe_out3 (vo_cnt_oneshot),
+//                   .probe_out4 (vo_tick_value),
+//                   .probe_out5 (vo_cnt_load_value),
+//                   .probe_out6 (vo_cnt_watermark)
+//                   );
 
-ila_vio_counter CL_VIO_ILA (
-                  .clk     (clk_main_a0),
-                  .probe0  (vi_tick),
-                  .probe1  (vi_cnt_ge_watermark),
-                  .probe2  (vi_tick_cnt),
-                  .probe3  (vi_cnt),
-                  .probe4  (vo_cnt_enable_q),
-                  .probe5  (vo_cnt_load_q),
-                  .probe6  (vo_cnt_clear_q),
-                  .probe7  (vo_cnt_oneshot_q),
-                  .probe8  (vo_tick_value_q),
-                  .probe9  (vo_cnt_load_value_q),
-                  .probe10 (vo_cnt_watermark_q)
-                  );
+// ila_vio_counter CL_VIO_ILA (
+//                   .clk     (clk_main_a0),
+//                   .probe0  (vi_tick),
+//                   .probe1  (vi_cnt_ge_watermark),
+//                   .probe2  (vi_tick_cnt),
+//                   .probe3  (vi_cnt),
+//                   .probe4  (vo_cnt_enable_q),
+//                   .probe5  (vo_cnt_load_q),
+//                   .probe6  (vo_cnt_clear_q),
+//                   .probe7  (vo_cnt_oneshot_q),
+//                   .probe8  (vo_tick_value_q),
+//                   .probe9  (vo_cnt_load_value_q),
+//                   .probe10 (vo_cnt_watermark_q)
+//                   );
 
 `endif //  `ifndef DISABLE_VJTAG_DEBUG
 
